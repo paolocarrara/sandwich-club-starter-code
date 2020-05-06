becd1c0354e69ac440c6ce702dff92a3514be998
Paolo Carrara <paolocarraratux@gmail.com>
2020-05-06 01:29:33 -0300

    Item (Sandwich) detail implementation.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,15 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -35,15 +38,17 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
+
+        if (position >= sandwiches.length) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
-        populateUI();
+        String json = sandwiches[position];
+        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +61,76 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        populateAlsoKnownAsTextViewUI(sandwich.getAlsoKnownAs());
+        populateDescriptionTextViewUI(sandwich.getDescription());
+        populatePlaceOfOriginTextViewUI(sandwich.getPlaceOfOrigin());
+        populateIngredientsTextViewUI(sandwich.getIngredients());
+    }
 
+    private void populateAlsoKnownAsTextViewUI (List<String> alsoKnownAs) {
+        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        TextView alsoKnownAsLabelTextView = findViewById(R.id.detail_also_known_as_label);
+
+        if (alsoKnownAs == null || alsoKnownAs.isEmpty()) {
+            alsoKnownAsTextView.setVisibility(View.GONE);
+            alsoKnownAsLabelTextView.setVisibility(View.GONE);
+        } else {
+            alsoKnownAsTextView.setText("");
+
+            int size = alsoKnownAs.size();
+            for (int i = 0; i < size; i++) {
+                alsoKnownAsTextView.append(alsoKnownAs.get(i));
+
+                if (i != size - 1) {
+                    alsoKnownAsTextView.append(", ");
+                }
+            }
+        }
+    }
+
+    private void populateDescriptionTextViewUI (String description) {
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+        TextView descriptionLabelTextView = findViewById(R.id.detail_description_label);
+
+        if (description == null || description.isEmpty()) {
+            descriptionTextView.setVisibility(View.GONE);
+            descriptionLabelTextView.setVisibility(View.GONE);
+        } else {
+            descriptionTextView.setText(description);
+        }
+    }
+
+    private void populatePlaceOfOriginTextViewUI (String placeOfOrigin) {
+        TextView placeOfOriginTextView = findViewById(R.id.origin_tv);
+        TextView placeOfOriginLabelTextView = findViewById(R.id.detail_place_of_origin_label);
+
+        if (placeOfOrigin == null || placeOfOrigin.isEmpty()) {
+            placeOfOriginTextView.setVisibility(View.GONE);
+            placeOfOriginLabelTextView.setVisibility(View.GONE);
+        } else {
+            placeOfOriginTextView.setText(placeOfOrigin);
+        }
+    }
+
+    private void populateIngredientsTextViewUI (List<String> ingredients) {
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
+        TextView ingredientsLabelTextView = findViewById(R.id.detail_ingredients_label);
+
+        if (ingredients == null || ingredients.isEmpty()) {
+            ingredientsTextView.setVisibility(View.GONE);
+            ingredientsLabelTextView.setVisibility(View.GONE);
+        } else {
+            ingredientsTextView.setText("");
+
+            int size = ingredients.size();
+            for (int i = 0; i < size; i++) {
+                ingredientsTextView.append(ingredients.get(i));
+
+                if (i != size - 1) {
+                    ingredientsTextView.append(", ");
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,97 @@
 package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.ArrayList;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        JSONObject jsonSandwich = null;
+        JSONObject name = null;
+        String mainName = null;
+        ArrayList<String> alsoKnownAs = new ArrayList<>();
+        String placeOfOrigin = null;
+        String description = null;
+        String image = null;
+        ArrayList<String> ingredients = new ArrayList<>();
+
+
+        try {
+            jsonSandwich = new JSONObject(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (jsonSandwich != null) {
+            try {
+                name = jsonSandwich.getJSONObject("name");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (name != null) {
+            try {
+                mainName = name.getString("mainName");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                JSONArray alsoKnownAsJSONArray = name.getJSONArray("alsoKnownAs");
+                for (int i = 0; i < alsoKnownAsJSONArray.length(); i++) {
+                    alsoKnownAs.add(alsoKnownAsJSONArray.get(i).toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (jsonSandwich != null) {
+            try {
+                placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (jsonSandwich != null) {
+            try {
+                description = jsonSandwich.getString("description");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (jsonSandwich != null) {
+            try {
+                image = jsonSandwich.getString("image");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (jsonSandwich != null) {
+            try {
+                JSONArray ingredientsJSONArray = jsonSandwich.getJSONArray("ingredients");
+                for (int i = 0; i < ingredientsJSONArray.length(); i++) {
+                    ingredients.add(ingredientsJSONArray.get(i).toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Sandwich sandwich = new Sandwich();
+        sandwich.setMainName(mainName);
+        sandwich.setAlsoKnownAs(alsoKnownAs);
+        sandwich.setPlaceOfOrigin(placeOfOrigin);
+        sandwich.setDescription(description);
+        sandwich.setImage(image);
+        sandwich.setIngredients(ingredients);
+
+        return sandwich;
     }
 }
